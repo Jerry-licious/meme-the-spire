80b9e6e8ff0ab6af4174781475d1921b2a0fb53e
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2021-02-20 21:57:38 -0500

    Removed unused import statement.

--- src/main/java/memethespire/relicmemes/RelicEquipPatch.java
+++ src/main/java/memethespire/relicmemes/RelicEquipPatch.java
@@ -3,7 +3,6 @@ package memethespire.relicmemes;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.rewards.RewardItem;
 
 @SpirePatch(clz = AbstractRelic.class, method = "onEquip")
 public class RelicEquipPatch {
